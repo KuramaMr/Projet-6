4a083e2b160ec9f602319be31f147c9307716180
Maquet Dorian <takenzmr@gmail.com>
2023-10-10 14:20:32 +0200

    Update

--- assets/script.js
+++ assets/script.js
@@ -29,33 +29,36 @@ const dots = document.querySelector(".dots");
 const dotList = document.querySelectorAll(".dot");
 
 const addSelected = () => {
-    for (let dot = 0; dot <= nbr__img; dot++) {
+    for (let dot = 0; dot < nbr__img; dot++) {
+      dotList[dot].classList.remove("dot_selected");
+
       if (dot === etape) {
         dotList[dot].classList.add("dot_selected");
-      } else {
-        dotList[dot].classList.remove("dot_selected");
       }
     }
   };
 
-
 suivant.addEventListener('click', function() {
     etape++;
+
     if(etape >= nbr__img) {
         etape = 0;
     }
+
     image.setAttribute("src","./assets/images/slideshow/" + slides[etape].image);
-	document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
+	  document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
     addSelected(etape);
 
 })
 
 precedent.addEventListener('click', function() {
     etape--;
+
     if(etape < 0) {
         etape = nbr__img - 1;
     }
+
     image.setAttribute("src","./assets/images/slideshow/" + slides[etape].image);
-	document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
+	  document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
     addSelected(etape);
 })
