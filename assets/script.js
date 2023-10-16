30df3221969325216417ce70f5be1dcdb257fd49
Maquet Dorian <takenzmr@gmail.com>
2023-10-16 11:16:58 +0200

    Finish

--- assets/script.js
+++ assets/script.js
@@ -28,37 +28,58 @@ let textInfo = document.getElementById("textInfo");
 const dots = document.querySelector(".dots");
 const dotList = document.querySelectorAll(".dot");
 
-const addSelected = () => {
-    for (let dot = 0; dot < nbr__img; dot++) {
-      dotList[dot].classList.remove("dot_selected");
 
-      if (dot === etape) {
-        dotList[dot].classList.add("dot_selected");
-      }
+/* Create bullet (dot) */ 
+function showDots() {
+  const dots = document.querySelector('.dots');
+  for (let j = 0; j < nbr__img; j++) {
+  const dot = document.createElement('span');
+  dot.id='span' + j;
+    dot.addEventListener('click', function(event) {
+      etape = Number(event.target.id.replace('span', ''));
+      showSlide();
+});
+
+  dot.classList.add('dot');
+  dots.appendChild(dot);
+  if (j == 0) { 
+    dots.children[j].classList.add('dot_selected');
   }
-};
+}}
+showDots();
 
-suivant.addEventListener('click', function() {
+
+/* Link bullet to image (dot) */
+function selected() {
+	const dot = document.getElementsByClassName('dot');
+	for (let etape = 0; etape < dot.length; etape++) {
+		dot[etape].classList.remove('dot_selected');
+	}
+	dot[etape].classList.add('dot_selected');
+ }
+
+/* Link tagline and image to HTML*/
+function showSlide() {
+  image.setAttribute("src","./assets/images/slideshow/" + slides[etape].image);
+	document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
+	selected();
+}
+
+/* Call function to move arrowleft */
+precedent.addEventListener("click", function () {
+	if (etape == 0) {
+		etape = nbr__img - 1;
+	}
+	else {
+		etape--;
+	}showSlide();
+});
+
+/* Call function to move arrowRight */	
+suivant.addEventListener("click", function () {
+	if (etape === nbr__img - 1) {
+		etape = 0;
+	} else {
     etape++;
-
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-
-    image.setAttribute("src","./assets/images/slideshow/" + slides[etape].image);
-	document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
-    addSelected(etape);
-
-})
-
-precedent.addEventListener('click', function() {
-    etape--;
-
-    if(etape < 0) {
-        etape = nbr__img - 1;
-    }
-
-    image.setAttribute("src","./assets/images/slideshow/" + slides[etape].image);
-	document.getElementById("textInfo").innerHTML = slides[etape].tagLine;
-    addSelected(etape);
-})
+	}showSlide();
+});
